c87eb74f93b54dcc8282e421e3203db2d96ef6e6
Igor <iwebwork.adm@gmail.com>
2021-04-28 00:09:38 -0300

    Ajustes pontuais

--- App.js
+++ App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 import {
-  SafeAreaView,
+  View,
   Text,
   StyleSheet
 } from 'react-native';
 import params from './src/params'
 import Field from './src/components/Field'
 
-
 export default class App extends Component {
   
   render() {
     return (
-      <SafeAreaView style={[style.container, style.textBig]}>
+      <View style={[style.container, style.textBig]}>
         <Text>App Campo minado Inciado</Text>
         <Text>{params.getRowsAmount()} x {params.getColumnsAmount()}</Text>
-        <Field />
         <Field opened nearMines={1} />
         <Field opened nearMines={2} />
         <Field opened nearMines={6} />
         <Field mined />
-      </SafeAreaView>
+        <Field mined opened/>
+        <Field mined opened exploded/>
+        <Field flagged/>
+        <Field flagged opened/>
+
+      </View>
     );
   }
 };
--- src/params.js
+++ src/params.js
@@ -2,8 +2,8 @@ import { Dimensions } from 'react-native'
 
 
 const params = {
-    blockSize: 50,
-    borderSize:2,
+    blockSize: 40,
+    borderSize:3,
     fontSize:38,
     headerRatio:0.15, // Representa o tamanho do painel superior na tela
     difficultLevel:0.1, // Nivel de dificultade (0.1 = Facil, 0.2 = Medio, 0.3 = Dificil)
